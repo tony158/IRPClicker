ca20d2219adb878731e7f519a7df51d9c0254881
tangx <tangxinzhi158@hotmail.com>
2021-12-16 22:47:34 +0100

    a little refactoring

--- main.py
+++ main.py
@@ -12,39 +12,14 @@ import random
 chrome_driver_path = "C:/webdrivers/chromedriver.exe"
 chrome_port = "localhost:8989"
 many_times = 300
-waiting_time = [15, 20]
+waiting_time = (12, 16)
 
-button_id_find_appointment = "btSrch4Apps"
-no_appointment_text_xpath = "//tr[contains(.,'No appointment(s) are currently available')]"
-
-
-def play_beep(duration=1000, count=2):
-    beep_count = 0
-    while beep_count < count:
-        winsound.Beep(440, duration)
-        beep_count += 1
-
-
-# return True, when available appointments are found or need to reload page
-def click_and_check_availability(driver):
-    button = driver.find_element(By.ID, button_id_find_appointment)
-    play_beep(300, 1)
-    button.click()
-
-    sleep(random.randint(waiting_time[0], waiting_time[1]))
-
-    try:
-        no_appointment_text = driver.find_element(By.XPATH, no_appointment_text_xpath)
-        if no_appointment_text.is_displayed():
-            return False
-    except NoSuchElementException:
-        return False
-    return True
+button_find_appointment = "btSrch4Apps"
+xpath_no_appointment_text = "//tr[contains(.,'No appointment(s) are currently available')]"
 
 
 def start_find_appointment():
     play_beep()
-
     opt = Options()
     opt.add_experimental_option("debuggerAddress", chrome_port)
     driver = webdriver.Chrome(service=Service(chrome_driver_path), options=opt)
@@ -59,11 +34,35 @@ def start_find_appointment():
     if appointment_available:
         print("********************found appointment! / or need to reload page!********************")
         play_beep(3000, 5)
-        exit()
+    else:
+        print("-----------------------no appointment found yet!----------------------------------")
+        play_beep(1000)
 
-    print("-----------------------no appointment found yet!----------------------------------")
-    play_beep(1000)
     exit()
+
+
+# return True, when available appointments are found or need to reload page
+def click_and_check_availability(driver):
+    button = driver.find_element(By.ID, button_find_appointment)
+    play_beep(300, 1)
+    button.click()
+
+    sleep(random.randint(waiting_time[0], waiting_time[1]))
+
+    try:
+        no_appointment_text = driver.find_element(By.XPATH, xpath_no_appointment_text)
+        if no_appointment_text.is_displayed():
+            return False
+    except NoSuchElementException:
+        return False
+    return True
+
+
+def play_beep(duration=1000, count=2):
+    beep_count = 0
+    while beep_count < count:
+        winsound.Beep(440, duration)
+        beep_count += 1
 
 
 # Press the green button in the gutter to run the script.
